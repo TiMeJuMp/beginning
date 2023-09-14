4a7a92ad98fd98e4058535ad39ac7abce920a930
Becker, Christian <christian.becker@tecalliance.net>
2023-09-14 07:51:42 +0200

    Aufgabenstellung Wiederholung Tag 1 Programmierung

--- Program.cs
+++ Program.cs
@@ -6,23 +6,16 @@ namespace HelloWorld
         {
         static void Main(string[] args)
         {
-            //Frage den Nutzer bis zu welcher Zahl aufsummiert werden soll
-            //Mache das in allen möglichen Schleifenarten, 
-            //In welcher geht es nicht und wieso?
-                //for, foreach, while, do while
-            
-            //6
-            //1 + 2 + 3 + 4 + 5 + 6 => 21
-            int ergebnis = 0;
-            int untilNumber = 0;
-            Console.WriteLine("Bis zu welcher Zahl soll aufsummiert werden?");
-            untilNumber = Convert.ToInt32(Console.ReadLine());
-            
-            for(int i = 0; i <= untilNumber; i++)
-            {
-                ergebnis += i;
-                Console.WriteLine($"Added number{i}, current result {ergebnis}");
-            }
-        } 
+            //Aufgabe 1: Initialisiere 2 Zahlen und gebe die Summe auf der Console aus
+            //Aufgabe 2: Lasse den Nutzer zwei Zahlen eingeben und gebe die Summe auf der Console aus
+            //Aufgabe 3: Lasse den Nutzer eine Zahl eingeben und gebe alle Zahlen 
+            //           bis einschließlich der eingegeben Zahl aus 
+            //Aufgabe 4: Lasse den Nutzer eine Zahl eingeben und gebe nur die geraden zahlen aus.
+            //           (Hinweis: Modulo operator. => Geteilt durch 2, wenn der Rest 0 ist, ist die Zahl gerade)
+            //Aufgabe 5: Lasse den Nutzer eine Zahl eingeben und berechne von dieser Zahl die Fakultät.
+            //Aufgabe 6a: Lasse den Nutzer einen Text eingeben und gebe den Text nur in Großbuchstaben aus
+            //Aufgabe 6b: Gebe die Länge des Textes aus 
+            //Aufgabe 6c: Zähle wie oft der Buchstabe a vorkommt.
+            //Aufgabe 6d: Gebe nur einen Teil des Textes aus. Ab Position 3, und dann die nächsten 5 Buchstaben
     }
 }
