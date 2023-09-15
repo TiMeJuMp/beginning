71946b31f32ca87148e2b1fb166d9afde89cd14e
Becker, Christian <christian.becker@tecalliance.net>
2023-09-15 11:45:04 +0200

    array example

--- Program.cs
+++ Program.cs
@@ -6,34 +6,22 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            // Aufgabe: Zahlenraten:
-            // Frage den Nutzer nach einer zahl zwischen 1 bis 100
-            // Erstelle eine RandomZahl => Hinweis 
-            Random random = new Random();
-            int numberToGuess = random.Next(0, 101);
-
-            Console.WriteLine("Bitte gebe eine Zahl ein, von welcher du denkst was die Zufallszahl ist.");
-            int userZahl = Convert.ToInt16(Console.ReadLine());
-
-            while(numberToGuess != userZahl)
+            //Frage den Nutzer wieviele Texte er in einem Array speichern will.
+            Console.WriteLine("Wieviele Texte willst du im Array speichern?");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            //Initalisiere das Array in der gewünschten größe
+            string[] myText = new string[numero];
+            //Lasse den Nutzer das Array nacheinander mit Texten befüllen.
+            for(int i=0; i<numero;i++)
             {
-                Console.WriteLine("Du hast die zahl nicht getroffen.");
-                if(userZahl > numberToGuess)
-                {
-                    Console.WriteLine("Die gesuchte Zahl ist kleiner als deine eingegebene!");
-                }
-                else
-                {
-                    Console.WriteLine("Die gesuchte Zahl ist größer als deine eingegebene!");
-                }
-                userZahl = Convert.ToInt16(Console.ReadLine());
+                string text1 = Console.ReadLine();
+                myText[i] = text1;
             }
-
-            Console.WriteLine("Du hast gewonnen!");
-
-            //Überprüft die zahl ob es numberToGuess ist
-            //wenn ja => hat der Spieler gewonnen
-            //Wenn nein => sage ob die gesuchte Zahl kleiner oder größer als die eingegebene Zahl ist.
+            //Gebe die Texte mit mindestens 20 Zeichen aus, fülle den Text mit Leerzeichen auf von Links wenn kleiner. 
+            foreach(string entry in myText)
+            {
+                Console.WriteLine(entry.PadLeft(20, ' '));
+            }
         }
     }
 }
