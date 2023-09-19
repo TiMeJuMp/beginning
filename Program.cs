ff9613fa6dd1c783bfb42c868401fb28fd79598c
Becker, Christian <christian.becker@tecalliance.net>
2023-09-19 08:26:23 +0200

    Sekundenrechen beispiel

--- Program.cs
+++ Program.cs
@@ -6,21 +6,23 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            //Lasse dir 2 Zahlen geben und lasse den den Rest ausgeben. 
-            //lese Zahl 1 ein 
-            Console.WriteLine("Gebe Zahl1 ein");
-            int numero1 = Convert.ToInt32(Console.ReadLine());
+            //Der Nutzer soll eine Zahl eingeben. Diese stellen die Anzahl der Sekunden dar. 
+            //Konvertiere die Zahl in die Anzahl der Tage, Stunden Minuten und restliche Sekunden.  
+            //(90.061 => 1 Tag 1 Stunde 1 Minute 1 Sekunde)
 
-            //lese Zahl 2 ein
-            Console.WriteLine("Gebe Zahl2 ein");
-            int numero2 = Convert.ToInt32(Console.ReadLine());
+            int amountSeconds = Convert.ToInt32(Console.ReadLine());
 
-            //Mache eine Division mit Rest und speicher das Ergebnis in einer variablen
-            int rest = numero1 % numero2;
+            int amountDays = amountSeconds / 24 / 60 / 60;
+            Console.WriteLine("Anzahl Tage:" + amountDays);
+            int restSeconds = amountSeconds % ( 24 * 60 * 60);
+            int amountHours = restSeconds / 60 / 60;
+            restSeconds = restSeconds % (60 * 60);
+            int amountMinutes = restSeconds / 60 ;
+            restSeconds = restSeconds % 60;
+            
+            Console.WriteLine($@"Aus der Anzahl {amountSeconds} wird {amountDays} Tage, {amountHours}
+                                Stunden, {amountMinutes} Minuten, {restSeconds} Sekunden");
 
-            //Gebe den Text aus
-            //"Die Division von {zahl1} und {Zahl2} erzeugt den Rest XYZ"
-            Console.WriteLine($"Die Division von {numero1} und {numero2} erzeugt den Rest {rest}");
         }
     }
 }
